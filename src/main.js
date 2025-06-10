3188ed4a2ef46dd795376ada710ad66e54b97c57
SilvinaAlvarez2811 <silvinaalvarez2811@gmail.com>
2025-06-10 17:57:46 -0300

    Estructura básica del proyecto y conexión a MongoDb con docker

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("UnaHur - Anti-Social net");
+const express = require("express");
+const conectarDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+conectarDB();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
