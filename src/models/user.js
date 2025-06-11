70a371a549a5a53f9f2c7ca9c14e3edbd6790df9
SilvinaAlvarez2811 <silvinaalvarez2811@gmail.com>
2025-06-11 16:29:40 -0300

    agrego modelo post

--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,27 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: [true, "userName es requerido"],
-    unique: [true, "ya existe ese userName"],
-  },
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "userName es requerido"],
+      unique: [true, "ya existe ese userName"],
+    },
 
-  email: {
-    type: String,
-    required: [true, "email es requerido"],
-    validate: {
-      validator: (t) => validator.isEmail(t),
-      message: (props) => `${props.value} no es un email válido`,
+    email: {
+      type: String,
+      required: [true, "email es requerido"],
+      validate: {
+        validator: (t) => validator.isEmail(t),
+        message: (props) => `${props.value} no es un email válido`,
+      },
     },
   },
-});
+  {
+    collection: "users",
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
