90c749a604eb1a848543e544a80961b2fb69fc41
SilvinaAlvarez2811 <silvinaalvarez2811@gmail.com>
2025-06-11 14:18:21 -0300

    Agrego modelos de User y Comment

--- src/models/postImage.js
+++ src/models/postImage.js
@@ -1,24 +1,22 @@
-const { mongoose } = require('../db/mongodb')
-const { Schema } = require('mongoose')
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const postImageSchema = new mongoose.Schema(
-    {
-        postId: {
-            type: Schema.Types.ObjectId,
-            ref: 'Post',
-            required: [true, 'postId es requerido']
-        },
-        imageUrl: {
-            type: [String],
-            required: [true, 'imageUrl es requerido']
-        }
-
-    },{
-        collection: 'postImages'
-    }
-
-)
-
+  {
+    postId: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      required: [true, "postId es requerido"],
+    },
+    imageUrl: {
+      type: [String],
+      required: [true, "imageUrl es requerido"],
+    },
+  },
+  {
+    collection: "postImages",
+  }
+);
 
 const PostImage = mongoose.model("PostImage", postImageSchema);
 module.exports = PostImage;
